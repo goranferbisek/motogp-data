25e65235c9a78e5e9d573f9b38e5d2e97c30f374
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-04-02 18:23:16 +0200

    Add rider model with a factory and a migration

---
+++ database/factories/RiderFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Rider;
+use Faker\Generator as Faker;
+
+$factory->define(Rider::class, function (Faker $faker) {
+    return [
+        'team_id' => factory(App\Team::class),
+        'motorcycle_id' => factory(App\Motorcycle::class),
+        'country_id' => factory(App\Country::class),
+        'racing_number' => $faker->numberBetween(1,99),
+        'age' => $faker->numberBetween(20,45)
+    ];
+});
---
+++ database/migrations/2020_04_02_155906_create_riders_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('riders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('team_id');
+            $table->foreignId('motorcycle_id');
+            $table->foreignId('country_id');
+            $table->unsignedSmallInteger('racing_number');
+            $table->unsignedtinyInteger('age');
+            $table->timestamps();
+
+            $table->foreign('team_id')
+                ->references('id')
+                ->on('teams')
+                ->onDelete('cascade');
+
+            $table->foreign('motorcycle_id')
+                ->references('id')
+                ->on('motorcycles')
+                ->onDelete('cascade');
+
+            $table->foreign('country_id')
+                ->references('id')
+                ->on('countries')
+                ->onDelete('cascade');
+
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('riders');
+    }
+}
