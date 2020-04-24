1397188c68a3cf1cddec0f9926a5470cf8973309
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-04-24 18:37:21 +0000

    Make a race track migration with model

---
+++ database/migrations/2020_04_24_183014_create_tracks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTracksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tracks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('country_id');
+            $table->string('name');
+            $table->smallInteger('length');
+            $table->timestamps();
+
+            $table->foreign('country_id')
+                ->references('id')
+                ->on('countries')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tracks');
+    }
+}
