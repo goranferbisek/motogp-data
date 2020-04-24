5ba10f3444d94642e22e6fe9d2ed357dbfce236c
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-04-24 20:49:13 +0200

    Make races table with model

---
+++ database/migrations/2020_04_24_184549_create_races_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRacesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('races', function (Blueprint $table) {
+            $table->id();
+            // add season id later
+            $table->foreignId('track_id');
+            // date
+            $table->timestamps();
+
+            $table->foreign('track_id')
+                ->references('id')
+                ->on('tracks')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('races');
+    }
+}
