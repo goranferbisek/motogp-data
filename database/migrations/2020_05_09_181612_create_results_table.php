d6562531e678349402b266afad74c577be42bbc9
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-05-09 20:32:19 +0200

    Add results model with migration

---
+++ database/migrations/2020_05_09_181612_create_results_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('race_id');
+            $table->foreignId('rider_id');
+            $table->tinyInteger('position');
+            $table->timestamps();
+
+            $table->foreign('race_id')
+                ->references('id')
+                ->on('races')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('results');
+    }
+}
