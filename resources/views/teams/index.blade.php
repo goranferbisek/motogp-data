d24937e456b1917020ea924a4e07e4b025c89cd7
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-05-10 20:00:04 +0200

    Add padding to teams view elements

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 <main>
-    <h1>Teams</h1>
+    <h1 class="pb-6">Teams</h1>
 
     @foreach ($teams as $team)
-        <h2>{{ $team->name }}</h2>
+        <h2 class="pt-6 pb-2 text-red-600">{{ $team->name }}</h2>
 
-        <ul>
+        <ul class="px-4">
             @foreach ($team->riders as $rider)
                 <li>{{ $rider->first_name . ' ' . $rider->last_name }}</li>
             @endforeach
