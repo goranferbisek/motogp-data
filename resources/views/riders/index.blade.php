fcbc8b99a59b43354aa09e463e66ae48ea09a1b1
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-05-11 18:42:18 +0200

    Make simple card for rider display

--- resources/views/riders/index.blade.php
+++ resources/views/riders/index.blade.php
@@ -2,18 +2,23 @@
 
 @section('content')
 <main>
-    <h1>Riders</h1>
+    <h1 class="text-xl text-red-600 text-center mb-6">Riders</h1>
 
-    <ul>
+    <div class="flex">
         @foreach ($riders as $rider)
-            <li>{{
-                $rider->racing_number .' | '.
-                $rider->first_name .' '.
-                $rider->last_name .' | '.
-                "Team: " . $rider->team->name
-            }}</li>
+            <div class="w-1/4 mx-4">
+                <div>Country |
+                    {{ $rider->first_name .' '.$rider->last_name .' '.
+                    $rider->racing_number .' age:'. $rider->age }}
+                </div>
+                <hr>
+                <img src="images/riders/vr46.jpg" alt="Valentino Rossi 46">
+                <div>
+                    Team: {{ $rider->team->name  }}
+                </div>
+            </div>
         @endforeach
-    </ul>
+    </div>
 
 </main>
 @endsection
