4ff2be3dc6cd4252837a9e9b7513f528470e286d
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-05-11 18:58:32 +0200

    Small layout fixes

--- resources/views/riders/index.blade.php
+++ resources/views/riders/index.blade.php
@@ -4,9 +4,9 @@
 <main>
     <h1 class="text-xl text-red-600 text-center mb-6">Riders</h1>
 
-    <div class="flex">
+    <div class="flex flex-wrap">
         @foreach ($riders as $rider)
-            <div class="w-1/4 mx-4">
+            <div class="w-1/4 mx-4 pb-4">
                 <div>Country |
                     {{ $rider->first_name .' '.$rider->last_name .' '.
                     $rider->racing_number .' age:'. $rider->age }}
@@ -14,7 +14,8 @@
                 <hr>
                 <img src="images/riders/vr46.jpg" alt="Valentino Rossi 46">
                 <div>
-                    Team: {{ $rider->team->name  }}
+                    Team: {{ $rider->team->name }}
+                    Bike: Some bike
                 </div>
             </div>
         @endforeach
