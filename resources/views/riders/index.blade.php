86964918e5c392423479047b0fd8a24fef13c3b9
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-05-14 21:52:38 +0200

    Fix card content layout

--- resources/views/riders/index.blade.php
+++ resources/views/riders/index.blade.php
@@ -3,33 +3,33 @@
 @section('content')
 <main>
     <h1 class="text-xl text-red-600 text-center mb-6">Riders</h1>
-
     <div class="grid xl:grid-cols-3 lg:grid-cols-2 gap-4">
         @foreach ($riders as $rider)
             <div class="flex">
-                <div class="w-1/3">
+                <div class="w-1/3 mr-2">
                     <img
                         src="images/riders/{{ $rider->photo ?: 'default-photo.jpg' }}"
                         alt="{{ $rider->first_name}} {{$rider->last_name}} {{$rider->racing_number}}"
-                        class=""
                     >
                 </div>
-                <div class="w-2/3 p-2">
+                <div class="w-2/3 text-right">
                     <img
                         src="images/flags/{{$rider->country->icon ?: 'default-flag.svg' }}"
                         alt="{{ $rider->country->name }}"
-                        class="h-4 mb-2"
+                        class="h-6 mb-2 ml-auto"
                     >
-                    {{ $rider->first_name}}
-                    {{$rider->last_name}}
-                    {{$rider->racing_number}}<br>
-                    Age: {{$rider->age }}<br>
-                    Team: {{ $rider->team->name }}<br>
-                    Bike: {{ $rider->bike->make }}<br>
+                    <span class="text-2xl">
+                        {{ $rider->first_name }}
+                        {{ $rider->last_name }}
+                        ({{ $rider->racing_number }})<br>
+                        Age: {{$rider->age }}<br>
+                    </span>
+
+                    <span class="text-sm">Team: {{ $rider->team->name }}<br></span>
+                    <span class="text-sm">Bike: {{ $rider->bike->make }}<br></span>
                 </div>
             </div>
         @endforeach
     </div>
-
 </main>
 @endsection
