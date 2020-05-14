d2937ac5145b6939b7f488a180aab14d8198ca4a
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-05-14 18:35:43 +0200

    Restyle the grid cards

--- resources/views/riders/index.blade.php
+++ resources/views/riders/index.blade.php
@@ -4,26 +4,28 @@
 <main>
     <h1 class="text-xl text-red-600 text-center mb-6">Riders</h1>
 
-    <div class="grid grid-cols-4 gap-4"> {{-- card container--}}
+    <div class="grid xl:grid-cols-3 lg:grid-cols-2 gap-4">
         @foreach ($riders as $rider)
-            <div class="bg-blue-200"> {{-- card --}}
-                <div class="flex items-center justify-between">
+            <div class="flex">
+                <div class="w-1/3">
+                    <img
+                        src="images/riders/{{ $rider->photo ?: 'default-photo.jpg' }}"
+                        alt="{{ $rider->first_name}} {{$rider->last_name}} {{$rider->racing_number}}"
+                        class=""
+                    >
+                </div>
+                <div class="w-2/3 p-2">
                     <img
                         src="images/flags/{{$rider->country->icon ?: 'default-flag.svg' }}"
                         alt="{{ $rider->country->name }}"
-                        class="h-4"
+                        class="h-4 mb-2"
                     >
-                    <span>{{ $rider->first_name}} {{$rider->last_name}} {{$rider->racing_number}}</span>
-                    <span>age: {{$rider->age }}</span>
-                </div>
-                <img
-                    src="images/riders/{{ $rider->photo ?: 'default-photo.jpg' }}"
-                    alt="{{ $rider->first_name}} {{$rider->last_name}} {{$rider->racing_number}}"
-                    class=""
-                >
-                <div>
-                    Team: {{ $rider->team->name }}
-                    Bike: {{ $rider->bike->make }}
+                    {{ $rider->first_name}}
+                    {{$rider->last_name}}
+                    {{$rider->racing_number}}<br>
+                    Age: {{$rider->age }}<br>
+                    Team: {{ $rider->team->name }}<br>
+                    Bike: {{ $rider->bike->make }}<br>
                 </div>
             </div>
         @endforeach
