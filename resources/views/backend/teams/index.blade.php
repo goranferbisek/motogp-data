fe92c52547fe74185c5a586e6c065fdb0834adba
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-09-01 21:49:03 +0200

    Style the teams admin list

--- resources/views/backend/teams/index.blade.php
+++ resources/views/backend/teams/index.blade.php
@@ -1,13 +1,14 @@
 @extends('backend.admin')
 
 @section('admin.content')
-    <h1>Teams</h1>
-    <p>Add a new racing team or edit an existing team</p>
+    <h1 class="text-xl mb-2">Teams</h1>
+    <p class="text-sm mb-6">Add a new racing team or edit an existing team</p>
 
     <div>
         @foreach ($teams as $team)
             <p>
                 {{ $team->name }}
+                <a href="admin/teams/edit/" class="text-blue-600">edit</a>
             </p>
         @endforeach
     </div>
