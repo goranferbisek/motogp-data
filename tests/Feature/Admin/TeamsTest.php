24ad2417a4deb7d6729ec0764f051fb08334b249
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-09-02 21:42:30 +0200

    Add a test for creating new teams

---
+++ tests/Feature/Admin/TeamsTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TeamsTest extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_add_a_new_team() {
+        $attributes = [
+            'name' => $this->faker->company
+        ];
+
+        $this->post('/admin/teams/create', $attributes);
+
+        $this->assertDatabaseHas('teams', $attributes);
+    }
+}
