9e31a9621f51b7f30bcc22403614a2d67b77eeea
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-09-23 18:50:01 +0200

    Add a view for countries list

---
+++ resources/views/backend/countries/index.blade.php
@@ -0,0 +1,21 @@
+@extends('backend.admin')
+
+@section('admin.content')
+    <h1 class="text-xl mb-2">Countries</h1>
+    <p class="text-sm mb-8">Add a new country or edit an existing one</p>
+
+    <a href="/admin/countries/create" class="bg-red-600 text-white px-2 py-4">
+        Add a new country
+    </a>
+
+    <div class="mt-8">
+        @foreach ($countries as $country)
+            <p class="mb-2">
+                {{ $country->name }}
+                <a href="/admin/countries/{{ $team->id }}/edit/" class="text-blue-600">
+                    edit
+                </a>
+            </p>
+        @endforeach
+    </div>
+@endsection
