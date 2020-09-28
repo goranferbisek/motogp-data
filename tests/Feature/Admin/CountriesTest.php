1c5faba4811d1d23cbbdb426595ac754bd788b60
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-09-28 22:28:12 +0200

    Add a new test for editing a country

--- tests/Feature/Admin/CountriesTest.php
+++ tests/Feature/Admin/CountriesTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Admin;
 
+use App\Country;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -27,5 +28,24 @@ class CountriesTest extends TestCase
         $this->assertDatabaseHas('countries', $attributes);
 
         $this->get('/admin/countries')->assertSee($attributes['name']);
+    }
+
+    /** @test */
+    public function a_user_can_edit_a_country()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->actingAs(factory(User::class)->create());
+
+        $country = factory(Country::class)->create();
+
+        // can you see edit form - assert see country name
+        $this->get('/admin/countries/' . $country->id . '/edit')
+            ->assertSee($country->name);
+
+        // change attrbiutes and assert redirect
+
+
+        // assert database see
     }
 }
