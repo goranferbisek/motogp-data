f0bf8b40c01098d0535bbf2bb93e02c2de3e7596
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-10-15 21:47:40 +0200

    Test if user can edit a bike

--- tests/Feature/BikesTest.php
+++ tests/Feature/BikesTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Bike;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -24,5 +25,24 @@ class BikesTest extends TestCase
         $this->post('/admin/bikes', $attributes)->assertRedirect('/admin/bikes');
         $this->assertDatabaseHas('bikes', $attributes);
         $this->get('/admin/bikes')->assertSee($attributes['make']);
+    }
+
+    /** @test */
+    public function a_user_can_edit_a_bike()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->actingAs(factory(User::class)->create());
+        $bike = factory(Bike::class)->create();
+
+        $this->get('/admin/bikes/' . $bike->id . '/edit')
+            ->assertSee($bike->make);
+
+        $this->put('/admin/bikes/' . $bike->id, $attributes =  [
+            'make' => 'Changed'
+            ])->assertRedirect('/admin/bikes');
+
+        $this->get('/admin/bikes/' . $bike->id . '/edit')->assertOk();
+        $this->assertDatabaseHas('bikes', $attributes);
     }
 }
