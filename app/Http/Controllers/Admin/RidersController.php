71b1762afed2f630412865beaf9caabd50d7c60e
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-10-20 20:40:26 +0200

    Validate and store a new rider

--- app/Http/Controllers/Admin/RidersController.php
+++ app/Http/Controllers/Admin/RidersController.php
@@ -24,4 +24,25 @@ class RidersController extends Controller
             'bikes' => Bike::all()
         ]);
     }
+
+    public function store()
+    {
+        $attributes = $this->validateRider();
+        $rider = new Rider($attributes);
+        $rider->save();
+
+        return redirect('/admin/riders');
+    }
+
+    public function validateRider()
+    {
+        return request()->validate([
+            'name' => 'required|unique:riders|max:255',
+            'team_id' => 'required|numeric',
+            'bike_id' => 'required|numeric',
+            'country_id' => 'required|numeric',
+            'racing_number' => 'required|numeric',
+            'age' => 'required|numeric',
+        ]);
+    }
 }
