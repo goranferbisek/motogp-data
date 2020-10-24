0350c3148d6cab11acf8509dc044059ea372e38b
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-10-24 12:20:48 +0200

    Add form validation on the view

--- resources/views/backend/riders/create.blade.php
+++ resources/views/backend/riders/create.blade.php
@@ -20,32 +20,61 @@
         @enderror
         <br>
         <label for="age">Age:</label><br>
-        <input type="text" name="age" id="age"><br>
-
-        <label for="country">Country:</label><br>
-        <select name="country" id="country">
+        <input
+            type="text"
+            name="age"
+            id="age"
+            class="border @if($errors->first('age')) border-red-600 @else border-blue-600 @endif"
+            value="{{ old('age') }}"
+        >
+        @error('age')
+            <p class="text-red-600">{{ $message }}</p>
+        @enderror
+        <br>
+        <label for="country_id">Country:</label><br>
+        <select name="country_id" id="country_id">
+                <option value="">--Select a country--</option>
             @foreach ($countries as $country)
                 <option value="{{ $country->id }}">{{ $country->name }}</option>
             @endforeach
         </select>
+        @error('country_id')
+            <p class="text-red-600">{{ $message }}</p>
+        @enderror
         <br>
-        <label for="number">Racing number:</label><br>
-        <input type="text" name="number" id="number"><br>
+        <label for="racing_number">Racing number:</label><br>
+        <input
+            type="text"
+            name="racing_number"
+            id="racing_number"
+            class="border @if($errors->first('racing_number')) border-red-600 @else border-blue-600 @endif"
+            value="{{ old('racing_number')}}">
+        <br>
+        @error('racing_number')
+            <p class="text-red-600">{{ $message }}</p>
+        @enderror
 
-        <hr>
-        <label for="team">Team:</label><br>
-        <select name="team" id="team">
+        <label for="team_id">Team:</label><br>
+        <select name="team_id" id="team_id">
+                <option value="">--Select a team--</option>
             @foreach ($teams as $team)
                 <option value="{{ $team->id }}">{{ $team->name }}</option>
             @endforeach
         </select>
+        @error('team_id')
+            <p class="text-red-600">{{ $message }}</p>
+        @enderror
         <br>
-        <label for="bike">Bike:</label><br>
-        <select name="bike" id="bike">
+        <label for="bike_id">Bike:</label><br>
+        <select name="bike_id" id="bike_id">
+            <option value="">--Select a bike--</option>
             @foreach ($bikes as $bike)
                 <option value="{{ $bike->id }}">{{ $bike->make }}</option>
             @endforeach
         </select>
+        @error('bike_id')
+            <p class="text-red-600">{{ $message }}</p>
+        @enderror
         <br>
 
         <input type="submit" value="Submit" class="mt-2 p-2 bg-blue-300">
