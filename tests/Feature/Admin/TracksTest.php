86c85eef45f6152e11847e599e3ea96de173df91
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-11-05 19:58:35 +0100

    Add a test for adding new tracks

---
+++ tests/Feature/Admin/TracksTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use App\Country;
+use App\Track;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TracksTest extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_add_a_track()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->actingAs(factory(User::class)->create());
+        $track = factory(Track::class)->create();
+        $attributes = [
+            'country_id' => factory(Country::class)->create()->id,
+            'name' => $this->faker->state .' raceway' . ' ' . $this->faker->country,
+            'length' => $this->faker->numberBetween(3500, 4500)
+        ];
+
+        $this->post('/admin/riders', $attributes)
+            ->assertRedirect('/admin/tracks');
+        $this->assertDatabaseHas('tracks', $attributes);
+        $this->get('/admin/tracks')->assertSee($attributes['name']);
+    }
+}
