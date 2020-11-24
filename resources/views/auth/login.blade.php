29e307ba6ce48950e404014a530d19eaf6f4e074
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-11-24 19:01:40 +0100

    Login form tweaks

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,43 +1,49 @@
 @extends('layout')
 
 @section('content')
-<div class="w-1/2 mx-auto border-2">
-    <div class="bg-gray-100 pl-2 py-2">Login</div>
+<div class="mx-auto w-5/6 md:w-3/5 xl:w-1/2 border-2">
+    <div class="bg-gray-200 pl-4 py-4">Login</div>
 
-    <div class="p-2">
+    <div class="px-6 py-4">
         <form method="POST" action="{{ route('login') }}">
             @csrf
 
-            <div class="pl-2">
-                <label for="email" class="">E-Mail Address</label>
+            <div class="w-3/4 mx-auto">
+                <label
+                    for="email"
+                    class="text-grey-darker text-sm font-bold"
+                >E-Mail Address</label>
 
-                <div class="col-md-6">
+                <div class="">
                     <input id="email"
                         type="email"
-                        class="border border-blue-600 @error('email') color-red-300 @enderror" name="email" value="{{ old('email') }}"
+                        class="w-full border border-blue-600 @error('email') color-red-300 @enderror" name="email" value="{{ old('email') }}"
                         required
                         autocomplete="email"
                         autofocus
                     >
 
                     @error('email')
-                        <span class="color-red-600">
+                        <span class="text-red-600">
                             <strong>{{ $message }}</strong>
                         </span>
                     @enderror
                 </div>
             </div>
 
-            <div class="pl-2">
-                <label for="password" class="">
+            <div class="w-3/4 mx-auto">
+                <label
+                    for="password"
+                    class="text-grey-darker text-sm font-bold mb-2"
+                >
                     Password
                 </label>
 
-                <div class="col-md-6">
+                <div class="">
                     <input
                         id="password"
                         type="password"
-                        class="border border-blue-600 @error('password') color-red-300 @enderror"
+                        class="w-full border border-blue-600 @error('password') border-red-300 @enderror"
                         name="password"
                         required
                         autocomplete="current-password"
@@ -51,21 +57,21 @@
                 </div>
             </div>
 
-            <div class="pl-2">
-                <div class="col-md-6 offset-md-4">
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+            <div class="w-3/4 mx-auto pt-2">
+                <div class="">
+                    <div class="">
+                        <input class="" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                        <label class="form-check-label" for="remember">
+                        <label class="" for="remember">
                             Remember Me
                         </label>
                     </div>
                 </div>
             </div>
 
-            <div class="pl-2">
-                <div class="flex items-center justify-evenly ">
-                    <button type="submit" class="mt-2 p-2 bg-blue-300">
+            <div class="pt-2">
+                <div class="flex items-center justify-evenly px-4">
+                    <button type="submit" class="px-6 py-2 bg-blue-300">
                         {{ __('Login') }}
                     </button>
 
